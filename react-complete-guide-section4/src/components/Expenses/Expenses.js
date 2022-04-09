0e64a63698670bc9e1c282e899a2d484f5daf9bd
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-04-09 15:23:05 +0900

    【セクション4】 62. Module Resources
    On branch udemy-react-complete-guide-section4
    Changes to be committed:
    	modified:   src/components/Expenses/Expenses.js

--- react-complete-guide-section4/src/components/Expenses/Expenses.js
+++ react-complete-guide-section4/src/components/Expenses/Expenses.js
@@ -9,8 +9,7 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
-    console.log("Expenses.js");
-    console.log(selectedYear);
+    setFilteredYear(selectedYear);
   };
 
   return (
