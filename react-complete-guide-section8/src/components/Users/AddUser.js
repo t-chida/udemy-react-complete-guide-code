47a11ef126236894c7e356c80f0d1301e8b8ab1c
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-09 15:19:00 +0900

    【セクション8】 98. Managing the Error State
    Changes to be committed:
    	modified:   src/components/UI/ErrorModal.js
    	modified:   src/components/Users/AddUser.js

--- react-complete-guide-section8/src/components/Users/AddUser.js
+++ react-complete-guide-section8/src/components/Users/AddUser.js
@@ -8,13 +8,23 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -25,13 +35,24 @@ const AddUser = (props) => {
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
   };
+
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occurred!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
