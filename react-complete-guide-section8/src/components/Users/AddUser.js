ab3d22a116bb78dbe59a6f9798ab7315199f4fd5
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-09 09:59:23 +0900

    【セクション8】 94. Adding Validation & Resetting Logic
    Changes to be committed:
    	modified:   src/components/Users/AddUser.js

--- react-complete-guide-section8/src/components/Users/AddUser.js
+++ react-complete-guide-section8/src/components/Users/AddUser.js
@@ -11,6 +11,13 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -24,9 +31,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Year)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
