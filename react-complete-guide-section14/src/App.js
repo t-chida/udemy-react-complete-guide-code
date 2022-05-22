74949b81c76a1d98550cd7a79d1f0984c057614a
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-23 08:03:02 +0900

    【セクション14】 182. Preparing The Project For The Next Steps
    On branch udemy-react-complete-guide-section14
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/AddMovie.js
    	new file:   src/components/AddMovie.module.css

--- react-complete-guide-section14/src/App.js
+++ react-complete-guide-section14/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -12,9 +13,11 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.py4e.com/api/films/");
+      const response = await fetch(
+        "https://react-http-tc-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
-        throw new Error("Something wrong!");
+        throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
@@ -38,6 +41,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
@@ -54,6 +61,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
