2e0c8a2cb781d5a9b9a0d3ad3f1c8f5d47cc375b
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-22 20:41:16 +0900

    【セクション12】 161. Optimizing with useMemo()
    On branch udemy-react-complete-guide-section12
    Changes to be committed:
    	modified:   react-complete-guide-section12/src/App.js
    	new file:   react-complete-guide-section12/src/components/Demo/DemoList.js
    	new file:   react-complete-guide-section12/src/components/Demo/DemoList.module.css

--- react-complete-guide-section12/src/App.js
+++ react-complete-guide-section12/src/App.js
@@ -1,23 +1,22 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from 'react';
 
-import Button from "./components/UI/Button/Button";
-import DemoOutput from "./components/Demo/DemoOutput";
-import "./App.css";
+import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
-  console.log("APP RUNNING");
-
-  const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
   }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
