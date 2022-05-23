271daaf3e894f09d1476cc9a26862ee3eceae655
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-23 22:26:21 +0900

    【セクション15】 193. Using the Custom Http Hook
    On branch udemy-react-complete-guide-section15-more-realistic
    Changes to be committed:
    	modified:   react-complete-guide-section15-more-realistic/src/App.js
    	renamed:    react-complete-guide-section15-more-realistic/src/components/hooks/use-http.js -> react-complete-guide-section15-more-realistic/src/hooks/use-http.js

--- react-complete-guide-section15-more-realistic/src/App.js
+++ react-complete-guide-section15-more-realistic/src/App.js
@@ -2,9 +2,31 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://react-http-tc-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
 
   useEffect(() => {
     fetchTasks();
--- react-complete-guide-section15-more-realistic/src/hooks/use-http.js
+++ react-complete-guide-section15-more-realistic/src/hooks/use-http.js
@@ -9,9 +9,9 @@ const useHttp = (requestConfig, applyData) => {
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        header: requestConfig.header,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
