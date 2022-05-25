f72fc2168b969c319dc37877f74e6f4b15a8e4e6
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-26 04:15:04 +0900

    【セクション20】 280. Adding a Layout Wrapper Component
    On branch udemy-react-complete-guide-section20-practice
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/layout/Layout.js
    	new file:   src/components/layout/MainNavigation.js

--- react-complete-guide-section20-practice/src/App.js
+++ react-complete-guide-section20-practice/src/App.js
@@ -3,23 +3,26 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuote from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuote />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuote />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
