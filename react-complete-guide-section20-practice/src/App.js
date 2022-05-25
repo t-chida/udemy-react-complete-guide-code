6150e5d03dd3968a93267bbb79981562f0b1922f
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-26 04:51:22 +0900

    【セクション20】 283. Adding a "Not Found" Page
    On branch udemy-react-complete-guide-section20-practice
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/pages/NotFound.js

--- react-complete-guide-section20-practice/src/App.js
+++ react-complete-guide-section20-practice/src/App.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuote from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
