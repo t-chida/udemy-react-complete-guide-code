1651fba98ff51bf5fcab52ec167871697d8d6af4
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-25 21:23:05 +0900

    【セクション20】 272. Adding Dynamic Routes with Params
    On branch udemy-react-complete-guide-section20
    Changes to be committed:
    	modified:   src/App.js
    	modified:   src/components/MainHeader.js
    	new file:   src/components/MainHeader.module.css
    	modified:   src/index.css
    	new file:   src/pages/ProductDetail.js
    	modified:   src/pages/Products.js

--- react-complete-guide-section20/src/App.js
+++ react-complete-guide-section20/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Route path="/products">
           <Products />
         </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
+        </Route>
       </main>
     </div>
   );
@@ -24,3 +28,4 @@ export default App;
 
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
