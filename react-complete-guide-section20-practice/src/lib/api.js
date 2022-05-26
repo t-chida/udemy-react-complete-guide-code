d709717fe14a9466024c28aee6ce695d1b8ad435
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-26 22:06:22 +0900

    【セクション20】 290. Adding the "Comments" Features
    On branch udemy-react-complete-guide-section20-practice
    Changes to be committed:
    	modified:   src/components/comments/Comments.js
    	modified:   src/components/comments/NewCommentForm.js
    	modified:   src/lib/api.js

--- react-complete-guide-section20-practice/src/lib/api.js
+++ react-complete-guide-section20-practice/src/lib/api.js
@@ -56,16 +56,13 @@ export async function addQuote(quoteData) {
 }
 
 export async function addComment(requestData) {
-  const response = await fetch(
-    `${FIREBASE_DOMAIN}/comments/${requestData.quoteId}.json`,
-    {
-      method: "POST",
-      body: JSON.stringify(requestData.commentData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${requestData.quoteId}.json`, {
+    method: 'POST',
+    body: JSON.stringify(requestData.commentData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const data = await response.json();
 
   if (!response.ok) {
