e884ff97733e601dc16296b1bff46c160cb2063e
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-27 20:24:56 +0900

    【セクション22】 309. Redirecting The User
    On branch udemy-react-complete-guide-section22
    Changes to be committed:
    	modified:   src/components/Auth/AuthForm.js
    	modified:   src/components/Profile/ProfileForm.js

--- react-complete-guide-section22/src/components/Profile/ProfileForm.js
+++ react-complete-guide-section22/src/components/Profile/ProfileForm.js
@@ -1,9 +1,10 @@
 import { useRef, useContext } from "react";
-
+import { useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -29,6 +30,8 @@ const ProfileForm = () => {
       }
     ).then((res) => {
       // assumption: Always succeed!
+
+      history.replace("/");
     });
   };
 
