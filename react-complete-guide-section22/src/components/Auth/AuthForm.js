3703852de7bfd346d57dcdc9c02d812cd5893e18
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-27 18:12:25 +0900

    【セクション22】 306. Adding User Login
    On branch udemy-react-complete-guide-section22
    Changes to be committed:
    	modified:   src/components/Auth/AuthForm.js

--- react-complete-guide-section22/src/components/Auth/AuthForm.js
+++ react-complete-guide-section22/src/components/Auth/AuthForm.js
@@ -22,35 +22,46 @@ const AuthForm = () => {
     // optional: Add validation
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBx0S5j-5_GnCnyWeE7CmcJi0oCC7FMbwk";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBx0S5j-5_GnCnyWeE7CmcJi0oCC7FMbwk",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBx0S5j-5_GnCnyWeE7CmcJi0oCC7FMbwk";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
+          return res.json();
         } else {
           res.json().then((data) => {
             let errorMessage = "Authentication failed!";
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
