7649db68f3f090aedfd2a9c467541c4e70a631ca
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-27 11:07:43 +0900

    【セクション22】 305. Showing Feedback To The User
    On branch udemy-react-complete-guide-section22
    Changes to be committed:
    	modified:   src/components/Auth/AuthForm.js
    	modified:   src/components/Auth/AuthForm.module.css

--- react-complete-guide-section22/src/components/Auth/AuthForm.js
+++ react-complete-guide-section22/src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -20,6 +21,7 @@ const AuthForm = () => {
 
     // optional: Add validation
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -36,11 +38,15 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
         } else {
           res.json().then((data) => {
-            // show an error modal
-            console.log(data);
+            let errorMessage = "Authentication failed!";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -65,7 +71,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request....</p>}
           <button
             type="button"
             className={classes.toggle}
