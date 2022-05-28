eb9e0c77385a1961b94d51da322ce716b3397c9d
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-29 01:00:28 +0900

    【セクション23】 339. Working with Params for SSG Data Fetching
    On branch udemy-react-complete-guide-section23-meetups
    Changes to be committed:
    	modified:   pages/[meetupId]/index.js
    	modified:   pages/index.js

--- react-complete-guide-section23-meetups/pages/[meetupId]/index.js
+++ react-complete-guide-section23-meetups/pages/[meetupId]/index.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
@@ -11,6 +11,27 @@ function MeetupDetails() {
       description="the meetup"
     />
   );
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: "m1",
+        title: "A First",
+        address: "dummy",
+        description: "the meetup",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- react-complete-guide-section23-meetups/pages/index.js
+++ react-complete-guide-section23-meetups/pages/index.js
@@ -23,6 +23,17 @@ function Homepage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: { meetups: DUMMY_MEETUPS },
+//   };
+// }
+
 export async function getStaticProps() {
   // fetch data from an API
   return {
