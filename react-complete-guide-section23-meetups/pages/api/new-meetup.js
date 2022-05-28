a76b75eadc6e98233ab696ccea67ac962e6d5319
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-29 02:26:34 +0900

    【セクション23】 342. Working with MongoDB
    On branch udemy-react-complete-guide-section23-meetups
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   pages/api/new-meetup.js

--- react-complete-guide-section23-meetups/pages/api/new-meetup.js
+++ react-complete-guide-section23-meetups/pages/api/new-meetup.js
@@ -1,11 +1,26 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://wheel:N7XFvss6n542C3fI@cluster0.0oqnl.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
