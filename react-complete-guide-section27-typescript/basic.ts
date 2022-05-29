9bc283d69fa1d71641d9cce7e24cdfa53619acad
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-30 08:07:07 +0900

    【セクション27】 404. Functions & Function Types
    On branch udemy-react-complete-guide-section27-typescript
    Changes to be committed:
    	modified:   basic.ts

--- react-complete-guide-section27-typescript/basic.ts
+++ react-complete-guide-section27-typescript/basic.ts
@@ -71,3 +71,13 @@ type Person = {
 };
 
 let personType: Person;
+
+// Functions & types
+
+function add2(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
