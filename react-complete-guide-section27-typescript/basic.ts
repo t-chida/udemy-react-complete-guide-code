fc78d90136da16ab59bec264b10ca7e40999614d
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-30 06:58:31 +0900

    【セクション27】 400. Working with Array & Object Types
    On branch udemy-react-complete-guide-section27-typescript
    Changes to be committed:
    	modified:   basic.ts

--- react-complete-guide-section27-typescript/basic.ts
+++ react-complete-guide-section27-typescript/basic.ts
@@ -15,3 +15,47 @@ userName = "Max";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let personAny: any;
+
+personAny = {
+  name: "Max",
+  age: 32,
+};
+
+personAny = {
+  isEmployee: true,
+};
+
+let person: { name: string; age: number };
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+people = [
+  {
+    name: "Max",
+    age: 32,
+  },
+  {
+    name: "Max",
+    age: 32,
+  },
+];
