5fbfc6272e021608d5f6901bfc0f92dd8283f1e4
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-05-31 06:20:25 +0900

    【セクション27】 418. The Context API & TypeScript
    On branch udemy-react-complete-guide-section27-react-ts
    Changes to be committed:
    	modified:   src/App.tsx
    	modified:   src/components/NewTodo.tsx
    	modified:   src/components/Todos.tsx
    	new file:   src/store/todos-context.tsx

--- react-complete-guide-section27-react-ts/src/components/NewTodo.tsx
+++ react-complete-guide-section27-react-ts/src/components/NewTodo.tsx
@@ -1,8 +1,11 @@
-import React, { useRef } from "react";
+import { useRef, useContext } from "react";
 
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -15,13 +18,13 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
     <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" ref={todoTextInputRef} />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
