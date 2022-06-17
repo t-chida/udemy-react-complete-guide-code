8df11f1f806f7ae7f12e3598e062011d25247115
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-17 22:01:13 +0900

    【セクション29】 463. Working with "props" & Dynamic Content
    On branch udemy-react-complete-guide-section29-react-course
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/Todo.js

--- react-complete-guide-section29-react-course/src/App.js
+++ react-complete-guide-section29-react-course/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
