7527e572063bfc76c3032307c4ee2ed7bcb4a8ff
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-17 22:14:56 +0900

    【セクション29】 465. Adding More Components
    On branch udemy-react-complete-guide-section29-react-course
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/Backdrop.js
    	new file:   src/components/Modal.js

--- react-complete-guide-section29-react-course/src/App.js
+++ react-complete-guide-section29-react-course/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -9,6 +9,8 @@ function App() {
       <Todo text="Learn React" />
       <Todo text="Master React" />
       <Todo text="Explore the full React course" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
